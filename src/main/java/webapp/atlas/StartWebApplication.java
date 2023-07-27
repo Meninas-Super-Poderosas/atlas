3ae097ff460b5163e71310af494e1bab651624f4
guilhermebeltrao <guilhermebeltraodev@protonmail.com>
2023-07-26 21:18:15 -0300

    feat: map added

---
+++ src/main/java/webapp/atlas/StartWebApplication.java
@@ -0,0 +1,7 @@
+package webapp.atlas;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartWebApplication {
+}
---
+++ src/main/java/webapp/atlas/controller/HomeController.java
@@ -0,0 +1,21 @@
+package webapp.atlas.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String main(Model model) {
+        return "home"; //view
+    }
+
+
+}
