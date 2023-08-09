d9f25d7633391081e855bc28b8d87599a27f14f1
Lucas Aurelio <zeaureliosv@gmail.com>
2023-08-08 23:01:20 -0300

    Feat:Add-post

---
+++ src/main/java/webapp/atlas/PostRepository.java
@@ -0,0 +1,7 @@
+package webapp.atlas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
---
+++ src/main/java/webapp/atlas/PostService.java
@@ -0,0 +1,46 @@
+package webapp.atlas;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public Post createPost(Post post){ return postRepository.save(post);}
+
+    public List<Post> getAllPosts(){ return postRepository.findAll();}
+
+    public Optional<Post> getPostByID(Long id){ return postRepository.findById(id);}
+
+    public Post updatePost(Long id, Post postDetails){
+        Optional<Post> post = postRepository.findById(id);
+        if(post.isPresent()){
+            Post existingPost = post.get();
+            existingPost.setTitle(postDetails.getTitle());
+            existingPost.setBody(postDetails.getBody());
+            return postRepository.save(existingPost);
+        }
+        return null;
+    }
+
+    public void deleteAllPosts(){postRepository.deleteAll();}
+
+    public void deletePost(Long id){ postRepository.deleteById(id);}
+
+    public Post updateLikeCount(Long id){
+        Optional<Post> post = postRepository.findById(id);
+        if(post.isPresent()){
+            Post existingPost = post.get();
+            existingPost.setLikes(existingPost.getLikes()+1);
+            return postRepository.save(existingPost);
+        }
+        return null;
+    }
+
+    }
