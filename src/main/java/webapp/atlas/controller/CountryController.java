5c2148197cef4d31e2ecb057468d78cae10cb196
guilhermebeltrao <guilhermebeltraodev@protonmail.com>
2023-08-19 21:35:05 -0300

    feat: create a single country method

--- src/main/java/webapp/atlas/controller/CountryController.java
+++ src/main/java/webapp/atlas/controller/CountryController.java
@@ -6,12 +6,20 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import webapp.atlas.model.Country;
 import webapp.atlas.service.CountryService;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static io.swagger.v3.oas.annotations.media.Schema.AccessMode.READ_ONLY;
 
@@ -49,5 +57,44 @@ public class CountryController {
     @PutMapping("/{id}")
     public Country updateCountry(@PathVariable Long id, @RequestBody Country country){
         return countryService.updateCountry(id, country);
+    }
+
+    @PostMapping("/create-all-countries")
+    public List<Country> createAllcountries() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
+                "https://restcountries.com/v3.1/name/Brazil?fields=name,flags,capital,population,currencies,languages",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Map<String, Object>>>() {}
+        );
+        List<Map<String, Object>> countryDataList = response.getBody();
+
+        List<Country> createdCountries = new ArrayList<>();
+
+        assert countryDataList != null;
+        for (Map<String, Object> countryData : countryDataList) {
+            Map<String, Object> flags = (Map<String, Object>) countryData.get("flags");
+            String flagUrl = (String) flags.get("png");
+
+            String name = (String) ((Map<String, Object>) countryData.get("name")).get("common");
+            Long population = Long.valueOf((Integer) countryData.get("population"));
+            String currency = (String) ((Map<String, Object>) ((Map<String, Object>) countryData.get("currencies")).values().iterator().next()).get("name");
+            String language = (String) ((Map<String, Object>) countryData.get("languages")).values().iterator().next();
+            String capital = ((List<String>) countryData.get("capital")).get(0);
+
+            Country country = new Country();
+            country.setName(name);
+            country.setPopulation(population);
+            country.setCurrencies(currency);
+            country.setLanguages(language);
+            country.setCapital(capital);
+            country.setFlag(flagUrl);
+
+            createdCountries.add(countryService.createCountry(country));
+
+        }
+
+        return createdCountries;
     }
 }
--- src/main/java/webapp/atlas/security/SpringSecurityConfig.java
+++ src/main/java/webapp/atlas/security/SpringSecurityConfig.java
@@ -25,7 +25,9 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        authorize.anyRequest().authenticated()
+                        authorize
+                                .requestMatchers("/countries/create-all-countries").permitAll()
+                                .anyRequest().authenticated()
                 ).formLogin(
                         form -> form
                                 .loginPage("/login")
