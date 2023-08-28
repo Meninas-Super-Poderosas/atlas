5d3232e7fb67b89d3ac81f25c6f3461c733d8cff
marantonieto <marcioantoni16@gmail.com>
2023-08-28 03:23:18 -0300

    FEAT-created-comment-view and likes

--- src/main/java/webapp/atlas/controller/HomeController.java
+++ src/main/java/webapp/atlas/controller/HomeController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import webapp.atlas.model.Comment;
 import webapp.atlas.model.Post;
 import webapp.atlas.model.Role;
 import webapp.atlas.model.User;
+import webapp.atlas.repository.CommentRepository;
 import webapp.atlas.repository.PostRepository;
 import webapp.atlas.repository.RoleRepository;
 import webapp.atlas.repository.UserRepository;
 import webapp.atlas.service.AuthService;
+import webapp.atlas.service.CommentService;
 import webapp.atlas.service.PostService;
 import webapp.atlas.service.UserService;
 
@@ -34,6 +37,9 @@ public class HomeController {
     private PostRepository postRepository;
 
     @Autowired
+    private CommentService commentService;
+
+    @Autowired
     private PostService postService;
 
     @Autowired
@@ -44,6 +50,9 @@ public class HomeController {
 
     @Autowired
     private RoleRepository roleRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
 
 
 
@@ -101,8 +110,25 @@ public class HomeController {
     @GetMapping("/post/{id}")
     public String showPost(Model model, @PathVariable Long id){
         Post post = postRepository.findById(id).get();
+        List<Comment> comments = commentService.getAllComments();
+        Comment comment = new Comment();
+        model.addAttribute("comment", comment);
         model.addAttribute("post", post);
+        model.addAttribute("comments", comments);
         return "show-post";
+    }
+
+    @PostMapping("/post/{id}/create-comment")
+    public String createComment(@PathVariable Long id, @ModelAttribute Comment formComment, Model model) {
+        Post post = postRepository.findById(id).get();
+        Comment comment = new Comment();
+        comment.setPost(post);
+        comment.setBody(formComment.getBody());
+        String username = authService.getCurrentUsername();
+        User user = userRepository.findByName(username).get();
+        comment.setUser(user);
+        commentRepository.save(comment);
+        return "redirect:/post/" + id;
     }
 
 
@@ -177,6 +203,17 @@ public class HomeController {
             return "redirect:/admin/users";
         }
         return "redirect:/";
+    }
+
+    @PostMapping("/increment-like")
+    public String incrementLike(@ModelAttribute("post") Post post) {
+        Post existingPost = postRepository.findById(post.getId()).orElse(null);
+
+        if (existingPost != null) {
+            existingPost.setLikes(existingPost.getLikes() + 1);
+            postRepository.save(existingPost);
+        }
+        return "redirect:/post/" + post.getId();
     }
 
     @GetMapping("/sign-up")
